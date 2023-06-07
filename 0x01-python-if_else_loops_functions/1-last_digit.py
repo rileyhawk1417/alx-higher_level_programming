efe8cc0a815ff5abbaf8b7559114f247bcc4673d
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-06-07 10:49:12 +0200

    fix: add negative test case

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastNumber = number % 10
+result = abs(number)
+lastNumber = result % 10
+
+if number < 0:
+    lastNumber *= -1
+
 if lastNumber > 5:
     print(f"Last digit of {number} is {lastNumber} and is greater than 5")
 elif lastNumber == 0:
@@ -9,5 +14,4 @@ elif lastNumber == 0:
 elif lastNumber < 6 and lastNumber != 0:
     print(f"Last digit of {number} is {lastNumber}", end=" ")
     print("and is less than 6 and not 0")
-else:
-    print(f"{lastNumber} not found in {number}")
+
