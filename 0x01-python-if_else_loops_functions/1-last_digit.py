002f28826037833091284e4e9df95b5f6e957ab2
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-06-07 11:11:02 +0200

    refactor: use .format style

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,10 +8,12 @@ if number < 0:
     lastNumber *= -1
 
 if lastNumber > 5:
-    print(f"Last digit of {number} is {lastNumber} and is greater than 5")
+    print("Last digit of {} is {} and is greater than 5"
+    .format((number), (lastNumber)))
 elif lastNumber == 0:
-    print(f"Last digit of {number} is {lastNumber} and is 0")
+    print("Last digit of {} is {} and is 0"
+    .format((number), (lastNumber)))
 elif lastNumber < 6 and lastNumber != 0:
-    print(f"Last digit of {number} is {lastNumber}", end=" ")
-    print("and is less than 6 and not 0")
+    print("Last digit of {} is {} "
+    "and is less than 6 and not 0".format((number), (lastNumber)))
 
