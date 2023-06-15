775f329776f46a7772d51e44f8845fe40eea5148
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-06-15 17:24:51 +0200

    fix: add another condition for ascending order

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -2,6 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * insert_node - Entry point
+ * @head: The head of the node
+ * @number: int value
+ * Return: Return the address of the newly added node
+ * Description: The function adds a number in the list.
+ * The number is added into the list in ascending order
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
 listint_t *newNode;
@@ -10,6 +18,15 @@ newNode = malloc(sizeof(listint_t));
 
 if (!newNode)
 	return (NULL);
+/*NOTE:If number is negative & less than the current number */
+if (currentNode == NULL || currentNode->n >= number)
+{
+newNode->next = currentNode;
+*head = newNode;
+return (newNode);
+}
+
+/*NOTE:If the number is less than current number but not negative */
 while (currentNode && currentNode->next && currentNode->next->n < number)
 	currentNode = currentNode->next;
 
