ee0a7dde1125c504502ae13f3f273f4b62c29cf0
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-06-15 23:30:48 +0200

    refactor: move code into __main__

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,20 +5,21 @@ if __name__ == "__main__":
     import sys
     import calculator_1
 
+    def ops(operator):
+        if operator == "+":
+            return calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
+        if operator == "-":
+            return calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
+        if operator == "*":
+            return calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
+        if operator == "/":
+            return calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
 
-def ops(operator):
-    if operator == "+":
-        return calculator_1.add(int(sys.argv[1]), int(sys.argv[3]))
-    if operator == "-":
-        return calculator_1.sub(int(sys.argv[1]), int(sys.argv[3]))
-    if operator == "*":
-        return calculator_1.mul(int(sys.argv[1]), int(sys.argv[3]))
-    if operator == "/":
-        return calculator_1.div(int(sys.argv[1]), int(sys.argv[3]))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
-
-print("{} {} {}".format(sys.argv[1], sys.argv[2], sys.argv[3]), end="")
-print(" = {}".format(ops(sys.argv[2])))
+    print("{} {} {}".format(sys.argv[1], sys.argv[2], sys.argv[3]), end="")
+    print(" = {}".format(ops(sys.argv[2])))
