95e755dc42c27d6bb50285ded8e7f4e83f1c91ee
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-06-15 13:26:41 +0200

    feat: complete task for list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - Entry point
+ * @list: listint_t value
+ * Return: int value
+ * Description: Returns 0 if no cycle is found
+ * returns 1 if cycle is found within list
+*/
+int check_cycle(listint_t *list)
+{
+listint_t *fasterNode = list;
+listint_t *normalNode = list;
+while (normalNode != NULL && fasterNode != NULL)
+{
+normalNode = normalNode->next;
+fasterNode = fasterNode->next->next;
+if (normalNode == fasterNode)
+	return (1);
+
+}
+return (0);
+}
