683863d02b1aff79d83a3e0067f5302715d04661
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-06-16 17:56:15 +0200

    chore: ensure cols cycles through the rows in matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@
 
 def print_matrix_integer(matrix=[[]]):
     for rows in matrix:
-        for cols in matrix:
+        for cols in rows:
             print("{:d}".format(cols), end=" " if cols != rows[-1] else "")
         print()
