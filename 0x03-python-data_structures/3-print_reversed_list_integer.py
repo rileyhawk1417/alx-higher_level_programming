35dfb962fcdf2ce9ae8cb1bc5d2b493b949506dc
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-06-16 16:41:01 +0200

    refactor: add condition if list is none

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
-    for items in range(length, 0, -1):
-        print("{:d}".format(items))
+    if my_list is None:
+        return None
+    else:
+        my_list.reverse()
+        for items in range(len(my_list)):
+            print("{:d}".format(my_list[items]))
