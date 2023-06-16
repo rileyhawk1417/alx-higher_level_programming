da61dcbb314b291e51c443fe8f25b16ce4e100cc
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-06-16 07:07:43 +0200

    refactor: ensure list items are printed from range

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for items in my_list:
-        print("{}".format(items))
+    # NOTE:Only print items in list range
+    for items in range(len(my_list)):
+        print("{:d}".format(my_list[items]))
