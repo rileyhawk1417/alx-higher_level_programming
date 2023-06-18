b13e374c3214347a11eb7dae20ae3fc3214c44a3
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-06-18 16:16:43 +0200

    fix: fix formatting style

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -5,4 +5,4 @@ def print_sorted_dictionary(a_dictionary):
     list_dict = list(a_dictionary.keys())
     list_dict.sort()
     for values in list_dict:
-        print("{} : {}".format(values, a_dictionary.get(values)))
+        print("{}: {}".format(values, a_dictionary.get(values)))
