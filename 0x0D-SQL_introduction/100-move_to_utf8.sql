91fd7d146b6641c4cb72a60944b8d19495a491a9
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-08-15 12:45:53 +0200

    fix: solve more typos

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- Use the hbtn_0c_0 db then alter first table character set
 USE `hbtn_0c_0`
-ALTER `first_table`
+ALTER TABLE `first_table`
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
