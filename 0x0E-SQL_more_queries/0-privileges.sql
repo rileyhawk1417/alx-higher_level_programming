d53d3c5ba57351af132f6144cda6159279eb1dfa
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-08-16 12:09:58 +0200

    test: replace single qoutes with backticks

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- Show user SQL privileges
-SHOW GRANTS FOR 'user_0d_1'@'localhost'; SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR `user_0d_1`@`localhost`;
+SHOW GRANTS FOR `user_0d_2`@`localhost`;
