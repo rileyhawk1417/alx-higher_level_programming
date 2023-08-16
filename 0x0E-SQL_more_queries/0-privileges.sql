c0ad212cd2ef19adad6be2e3eaead38c210336ad
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-08-16 12:05:23 +0200

    test: reduce line number

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,2 @@
 -- Show user SQL privileges
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost'; SHOW GRANTS FOR 'user_0d_2'@'localhost';
