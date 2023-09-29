bd6b521f8aa3b85b04bf2526888cdcf48973240c
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-09-29 14:45:21 +0200

    fix: reduce file to 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Get content length using curl then print it
-# Not sure on how to use grep on this
+# Get content length using curl then print it, unsure on how to use grep
 curl -sI "$1" | awk '/Content-Length/{print $2}'
