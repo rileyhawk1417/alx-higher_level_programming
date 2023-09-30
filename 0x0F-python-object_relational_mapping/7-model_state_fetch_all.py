0fa98d2d5a59bfdc457b490fdfa2a2ce61fdd70b
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-09-30 14:31:06 +0200

    fix: make files executable

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -14,5 +14,6 @@ if __name__ == "__main__":
     InitSession = create_session(bind=sql_engine)
     session = InitSession()
 
-    for states in session.query(State).order_by(State.id):
-        print("{}: {}".format(states.id, states.name))
+    for states_id, states_name in session.query(State.id,
+                                                State.name).order_by(State.id):
+        print("{}: {}".format(states_id, states_name))
