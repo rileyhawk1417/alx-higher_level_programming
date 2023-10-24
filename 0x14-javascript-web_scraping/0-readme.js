b822d3e19f98a46463f65d158fbef2978d421698
Sean Riley Hawkins <rileyhawk249@gmail.com>
2023-10-24 14:53:00 +0200

    fix: remove path & just use argv

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 /* Read the file contents from a given file */
 const fs = require('fs');
-const { argv } = require('process');
-const path = require('path');
-const filePath = path.join(__dirname, argv[2]);
-fs.readFile(filePath, { encoding: 'utf-8' }, function (err, data) {
+const args = process.argv;
+fs.readFile(args[2], { encoding: 'utf-8' }, function (err, data) {
   if (err) {
     console.log(err);
   } else {
